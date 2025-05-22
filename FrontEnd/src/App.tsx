be30e45343fbf6d685a2cdd982a0f8334f53d993
Sebastian Caballero <sebastianscmartinez@gmail.com>
2025-05-22 11:27:52 -0500

    Se Modifica rutas app.tsx

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -5,15 +5,13 @@ import './App.css'
 
 
 //rutas por crear SEBASTIAN MUÑOZ
-//import LoginEmail from './Components/LoginEmail/LoginEmail.tsx' //--> Muckup Iniciar Sesion - correo
-//import LoginPassword from './Components/LoginPassword/LoginPassword.tsx' //--> Muckup Iniciar-contraseña
+
 // import LoginEmail from './Components/LoginEmail/LoginEmail.tsx' //--> Muckup Iniciar Sesion - correo
 // import LoginPassword from './Components/LoginPassword/LoginPassword.tsx' //--> Muckup Iniciar-contraseña
 
 
 //Rutas por crear SAMUEL ALVAREZ
-// import Suscription from './Components/Suscription/Suscription.tsx' //--> Muckup Suscripcion
-//import Register from './Components/Register/Register.tsx'  //--> Mcukup Registrar
+
 //import Suscription from './Components/Suscription/Suscription.tsx' //--> Muckup Suscripcion
 // import Register from './Components/Register/MainRegister.tsx'  //--> Mcukup Registrar
 //import RegisterPassword from './Components/RegisterPassword/RegisterPassword.tsx' //--> Muckup Registro-contraseña
@@ -23,6 +21,7 @@ import ProductCard from './Components/ProductCart/ProductCart.tsx' //--> Muckup 
 import Profile from './Components/Profile/Profile.tsx' //--> Muckup Perfil
 import LoginNavbar from './Components/LoginNavbar/LoginNavbar.tsx'
 import Menu from './Components/LoggedNav/LoggedNav.tsx'
+
 //Rutas por crear JULIANA MORALES
 //import PasswordRecovery from './Components/PasswordRecovery/PasswordRecovery.tsx' //--> Muckup Recuperacion- constraseña
 //import ConfirmPassword from './Components/ConfirmPassword/ConfirmPassword.tsx' //--> Muckup Confirmar-verficacion
@@ -36,12 +35,6 @@ import Menu from './Components/LoggedNav/LoggedNav.tsx'
 //Rutas por crear JULIANA MORALES
 //import PasswordRecovery from './Components/PasswordRecovery/PasswordRecovery.tsx' //--> Muckup Recuperacion- constraseña
 //import ConfirmPassword from './Components/ConfirmPassword/ConfirmPassword.tsx' //--> Muckup Confirmar-verficacion
-
-//Rutas por crear BRAYAN
-// import Purchases from './Components/Purchases/Purchases.tsx' //--> Muckup Mis ventas
-// import Sales from './Components/Sales/Sales.tsx' //--> Muckup Mis ventas
-// import OnSale from './Components/OnSale/OnSale.tsx' //--> Muckup Previzualizacion
-// import ProductRegistrationForm from './Components/PublicarTest/PublicarTest.tsx'
 
 
 function App() {
