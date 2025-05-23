bcb2783582dc83832c9952b5f10af5a02d4d365a
Sebastian Caballero <sebastianscmartinez@gmail.com>
2025-05-23 10:08:20 -0500

    Se migra la base de datos localmente

--- BackEnd/main_app/settings.py
+++ BackEnd/main_app/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mercalista',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
