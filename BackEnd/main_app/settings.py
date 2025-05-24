bbd1afb5ecd6873190f5e4f91328fdf3e6fc8f22
Sebastian Caballero <sebastianscmartinez@gmail.com>
2025-05-23 20:38:27 -0500

    Djando Data Bases

--- BackEnd/main_app/settings.py
+++ BackEnd/main_app/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'main_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mercalista',
+        'USER': 'root',
+        'PASSWORD': 'sWMMWkJIRzYoQyOXoosovwwjFmoHsqUu',
+        'HOST': 'gondola.proxy.rlwy.net',
+        'PORT': '19744',
     }
 }
 
