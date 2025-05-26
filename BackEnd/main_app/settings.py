fbc1ec012a81a572c1ba8f47e957dce2044fa06f
Sebastian Caballero <sebastianscmartinez@gmail.com>
2025-05-26 09:53:59 -0500

    Se corrige cambios de la base de datos

--- BackEnd/main_app/settings.py
+++ BackEnd/main_app/settings.py
@@ -95,9 +95,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'railway',
         'USER': 'root',
-        'PASSWORD': 'NOWzYqQHTqpRsvqgnROFrduVUxjxdOun',
+        'PASSWORD': 'QsXJxYSmzQoSBPRjEMddZGBJWybqfyib',
         'HOST': 'mainline.proxy.rlwy.net',
-        'PORT': '47421',
+        'PORT': '12810',
     }
 }
 
