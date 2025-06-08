3a19a790c59152d1dea43abc26b3bd6b8ce01ac8
Brayan <puertacuartasbrayan2000@gmail.com>
2025-06-08 00:58:01 -0500

    se descomenta vista y ruta de vista para que funcione

--- BackEnd/producto/urls.py
+++ BackEnd/producto/urls.py
@@ -1,8 +1,8 @@
 #from . import views
 from django.urls import path, include
-from .views import ProductosEnVenta #crearProducto
+from .views import ProductosEnVenta, crearProducto
 
 urlpatterns = [
     path("verProductos/<int:id>", ProductosEnVenta.as_view(), name='producto-list'),
-    #path("crearProducto/<int:id>", crearProducto.as_view(), name='crear-producto'),
+    path("crearProducto/<int:id>", crearProducto.as_view(), name='crear-producto'),
 ]
