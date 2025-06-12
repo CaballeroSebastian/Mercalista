eebe3ab0365423455e660ccb482d3e3e03d4aaa8
samulalva <samuel1a.alvarez@gmail.com>
2025-06-12 00:31:39 -0500

    Crea usuario en comprador o vendedor dependiendo de la info al registrar una cuenta

--- BackEnd/Register/urls.py
+++ BackEnd/Register/urls.py
@@ -1,10 +1,9 @@
 #from . import views
 from django.urls import path, include
 from .views import CrearUsuario
-from .views import VerificarDatosView, CrearTipoDeUsuario
+from .views import VerificarDatosView
 
 urlpatterns = [
     path("crearUsuario/", CrearUsuario.as_view(), name='crear-usuario'),
     path('verificarDatos/', VerificarDatosView.as_view(), name='verificar_datos'),
-    path('crear-tipo-usuario/', CrearTipoDeUsuario.as_view(), name='crear_tipo_usuario'),
 ]
