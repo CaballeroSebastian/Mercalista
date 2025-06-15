520df5ee459c6e881007ea2f8cb991b9b7cec94e
Sebastian Caballero <sebastianscmartinez@gmail.com>
2025-06-14 21:46:27 -0500

    Se vuelve a crear profile y su backend

---
+++ BackEnd/usersProfile/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import UsersProfileView, UserProfileByCedulaView, ProfileImageUpdateView
+
+urlpatterns = [
+    path('', UsersProfileView.as_view()),
+    path('<str:cedula>/', UserProfileByCedulaView.as_view()),
+    path('update-image/<str:cedula>/', ProfileImageUpdateView.as_view()),
+]
+
